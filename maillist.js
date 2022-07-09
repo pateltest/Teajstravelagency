255ad6e3d2038e379c6b325244d0121cd93510ba
pateltest <108990187+pateltest@users.noreply.github.com>
2022-07-09 10:44:19 -0500

    fix

--- maillist.js
+++ maillist.js
@@ -10,12 +10,12 @@ function readForm()
 		
 		if(document.emaillist.radTerms[0].checked)
 		{
-			document.write("Thank you, " + FullName + ". Your email address will not be added to Patel's Travels e-mailing list.");
+			document.write("Thank you, " + FullName + ". Your email address will not be added to Tejas's Travels e-mailing list.");
 		}
 			
 		else if(document.emaillist.radTerms[1].checked)
 		{
-			document.write("Thank you, " + FullName + ". You have been added to Patel's Travels e-mailing list and we have your email as " + nofakeemail + ".");
+			document.write("Thank you, " + FullName + ". You have been added to Tejas's Travels e-mailing list and we have your email as " + nofakeemail + ".");
 		}
 		}
 		
